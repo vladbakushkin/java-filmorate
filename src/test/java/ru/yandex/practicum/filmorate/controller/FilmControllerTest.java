66e3c21c63b119682af21a3e95f4877169f8568c
Vladislav <bakushkinv@yandex.ru>
2024-02-18 21:52:29 +0300

    feat: добавить тесты для новых методов контроллеров

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -2,18 +2,21 @@ package ru.yandex.practicum.filmorate.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
@@ -21,14 +24,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(FilmController.class)
+//@WebMvcTest(FilmController.class)
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class FilmControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
     void test1_addFilm() throws Exception {
@@ -147,16 +159,15 @@ class FilmControllerTest {
                 "1989-04-17", 190);
         updatedFilm.setId(9999);
 
-        Objects.requireNonNull(mockMvc.perform(
+        mockMvc.perform(
                         put("/films")
                                 .content(objectMapper.writeValueAsString(updatedFilm))
                                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isInternalServerError())
-                .andExpect(result -> assertInstanceOf(ValidationException.class, result.getResolvedException())));
+                .andExpect(status().isNotFound())
+                .andExpect(result -> assertInstanceOf(FilmNotFoundException.class, result.getResolvedException()));
     }
 
     @Test
-    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
     void test8_getAllFilms() throws Exception {
         Film film1 = new Film("film1", "film1",
                 "1967-03-25", 100);
@@ -197,5 +208,253 @@ class FilmControllerTest {
         assertEquals(film2.getDescription(), filmList.get(1).getDescription(), "Неверное описание фильма.");
         assertEquals(film2.getReleaseDate(), filmList.get(1).getReleaseDate(), "Неверная дата релиза фильма.");
         assertEquals(film2.getDuration(), filmList.get(1).getDuration(), "Неверная продолжительность фильма.");
+    }
+
+    @Test
+    void test9_getFilmById1() throws Exception {
+        Film film = new Film("film", "film",
+                "1967-03-25", 100);
+
+        mockMvc.perform(
+                        post("/films")
+                                .content(objectMapper.writeValueAsString(film))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        MvcResult mvcResult = mockMvc.perform(get("/films/1"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseJson = mvcResult.getResponse().getContentAsString();
+        Film film1 = objectMapper.readValue(responseJson, Film.class);
+
+        assertEquals(film.getName(), film1.getName(), "Неверное название фильма.");
+        assertEquals(film.getDescription(), film1.getDescription(), "Неверное описание фильма.");
+        assertEquals(film.getReleaseDate(), film1.getReleaseDate(), "Неверная дата релиза фильма.");
+        assertEquals(film.getDuration(), film1.getDuration(), "Неверная продолжительность фильма.");
+    }
+
+    @Test
+    void test10_getFilmUnknownWithId9999() throws Exception {
+        Film film = new Film("film", "film",
+                "1967-03-25", 100);
+
+        mockMvc.perform(
+                        post("/films")
+                                .content(objectMapper.writeValueAsString(film))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/films/9999"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void test11_addLike() throws Exception {
+        User user = new User("mail@mail.ru", "dolore", "Nick Name", "1946-08-20");
+
+        Film film1 = new Film("film1", "film1", "1967-03-25", 90);
+        Film film2 = new Film("film2", "film2", "1967-03-25", 150);
+        Film film3 = new Film("film3", "film3", "1967-03-25", 100);
+
+        mockMvc.perform(
+                        post("/users")
+                                .content(objectMapper.writeValueAsString(user))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        post("/films")
+                                .content(objectMapper.writeValueAsString(film1))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        post("/films")
+                                .content(objectMapper.writeValueAsString(film2))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        post("/films")
+                                .content(objectMapper.writeValueAsString(film3))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        put("/films/2/like/1"))
+                .andExpect(status().isOk());
+
+        MvcResult mvcResult = mockMvc.perform(
+                        get("/films/popular?count=1"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseJson = mvcResult.getResponse().getContentAsString();
+
+        List<Film> filmList = objectMapper.readValue(responseJson, new TypeReference<>() {
+        });
+
+        assertEquals(1, filmList.size(), "Неверное количество фильмов в списке.");
+
+        assertEquals(film2.getName(), filmList.get(0).getName(), "Неверное название фильма.");
+        assertEquals(film2.getDescription(), filmList.get(0).getDescription(), "Неверное описание фильма.");
+        assertEquals(film2.getReleaseDate(), filmList.get(0).getReleaseDate(), "Неверная дата релиза фильма.");
+        assertEquals(film2.getDuration(), filmList.get(0).getDuration(), "Неверная продолжительность фильма.");
+        assertEquals(1, filmList.get(0).getLikes().size(), "Неверное количество лайков.");
+    }
+
+    @Test
+    void test12_removeLike() throws Exception {
+        User user = new User("mail@mail.ru", "dolore", "Nick Name", "1946-08-20");
+
+        Film film = new Film("film1", "film1", "1967-03-25", 90);
+
+        mockMvc.perform(
+                        post("/users")
+                                .content(objectMapper.writeValueAsString(user))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        post("/films")
+                                .content(objectMapper.writeValueAsString(film))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        put("/films/1/like/1"))
+                .andExpect(status().isOk());
+
+        MvcResult mvcResult1 = mockMvc.perform(
+                        get("/films/1"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseJson1 = mvcResult1.getResponse().getContentAsString();
+        Film film1WithLike = objectMapper.readValue(responseJson1, Film.class);
+
+        assertEquals(1, film1WithLike.getLikes().size(), "Неверное количество лайков.");
+
+        mockMvc.perform(
+                        delete("/films/1/like/1"))
+                .andExpect(status().isOk());
+
+        MvcResult mvcResult2 = mockMvc.perform(
+                        get("/films/1"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseJson2 = mvcResult2.getResponse().getContentAsString();
+        Film film1WithoutLike = objectMapper.readValue(responseJson2, Film.class);
+
+        assertEquals(0, film1WithoutLike.getLikes().size(), "Неверное количество фильмов в списке.");
+    }
+
+    @Test
+    void test13_removeLikeFromUnknownUser() throws Exception {
+        User user = new User("mail@mail.ru", "dolore", "Nick Name", "1946-08-20");
+
+        Film film = new Film("film1", "film1", "1967-03-25", 90);
+
+        mockMvc.perform(
+                        post("/users")
+                                .content(objectMapper.writeValueAsString(user))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        post("/films")
+                                .content(objectMapper.writeValueAsString(film))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        put("/films/1/like/1"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        delete("/films/1/like/-2"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void test14_getAllPopularFilms() throws Exception {
+        User user = new User("mail@mail.ru", "dolore", "Nick Name", "1946-08-20");
+
+        for (int i = 0; i < 6; i++) {
+            mockMvc.perform(
+                            post("/users")
+                                    .content(objectMapper.writeValueAsString(user))
+                                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        }
+
+        Film film = new Film("film1", "film1", "1967-03-25", 90);
+
+        for (int i = 0; i < 15; i++) {
+            mockMvc.perform(
+                            post("/films")
+                                    .content(objectMapper.writeValueAsString(film))
+                                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        }
+
+        mockMvc.perform(
+                        put("/films/6/like/1"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        put("/films/6/like/2"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        put("/films/6/like/3"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        put("/films/9/like/4"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        put("/films/9/like/5"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(
+                        put("/films/14/like/6"))
+                .andExpect(status().isOk());
+
+        MvcResult mvcResult = mockMvc.perform(
+                        get("/films/popular"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseJson = mvcResult.getResponse().getContentAsString();
+        List<Film> filmList = objectMapper.readValue(responseJson, new TypeReference<>() {
+        });
+
+        assertEquals(10, filmList.size(), "Неверное количество фильмов в списке.");
+
+        assertEquals(3, filmList.get(0).getLikes().size(), "Неверное количество лайков.");
+        assertEquals(2, filmList.get(1).getLikes().size(), "Неверное количество лайков.");
+        assertEquals(1, filmList.get(2).getLikes().size(), "Неверное количество лайков.");
+        assertEquals(0, filmList.get(3).getLikes().size(), "Неверное количество лайков.");
+
+        MvcResult mvcResult2 = mockMvc.perform(
+                        get("/films/popular?count=5"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseJson2 = mvcResult2.getResponse().getContentAsString();
+        List<Film> filmList2 = objectMapper.readValue(responseJson2, new TypeReference<>() {
+        });
+
+        assertEquals(5, filmList2.size(), "Неверное количество фильмов в списке.");
+
+        assertEquals(3, filmList2.get(0).getLikes().size(), "Неверное количество лайков.");
+        assertEquals(2, filmList2.get(1).getLikes().size(), "Неверное количество лайков.");
+        assertEquals(1, filmList2.get(2).getLikes().size(), "Неверное количество лайков.");
+        assertEquals(0, filmList2.get(3).getLikes().size(), "Неверное количество лайков.");
+        assertEquals(0, filmList2.get(4).getLikes().size(), "Неверное количество лайков.");
     }
 }
